a0641801fd18d747dcf1db0a11f0a08c813f3034
Isaac D. Arcilla <isaacdarcilla@gmail.com>
2016-11-22 15:18:42 +0800

    Add files via upload

---
+++ Main/headers/header.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#CONFIG_FILE
+PKG_MODULES=18
+
+verde="\033[1;32m"
+azul="\033[1;34m"
+rescolor="\e[0m"
+echo " "
+echo -e  "\e[1;31m        ▄█████▄\e[1;34m                                 ██  ██ "
+echo -e  "\e[1;31m        ██\e[1;34m                                          ██   "
+echo -e  "\e[1;31m       █████\e[1;34m ▄████▄ ▄████▄ ▄████▄ ██  ██ ▄████▄ ██ █████ ██    ██"
+echo -e  "\e[1;31m        ██\e[1;34m   ██     ██   █ ██     ██  ██ ██   █ ██  ██    ██  ██"
+echo -e  "\e[1;31m        ██\e[1;34m    ▀▀▀██ ██▀▀▀  ██     ██  ██ ██     ██  ██     ████          "
+echo -e  "\e[1;31m        ██\e[1;34m   ▀████▀ ▀████▀ ▀████▀ ▀████▀ ██     ██  ▀███     █▀           "
+echo -e  "\e[1;32m	Free Security Team est. 2015 [$PKG_MODULES]\e[1;34m                 ▄▄██ "
+echo -e "	To view  more options.  Type 'help' in  the command line. " $rescolor""
+echo " "
